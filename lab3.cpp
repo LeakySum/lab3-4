fbdbc5a4705106c6a544c16e795016448f3f25f1
LeakySum <leakysum60@gmail.com>
2016-11-16 10:28:47 +0300

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -1,22 +1,20 @@
-+#include <iostream>
- +#include <math.h>
- +
- +using namespace std;
- +
- +int main() {
- +    double E=1e-6;
- +    double Sum=0;
- +    double sd;
- +    double si;
- +    int k=0;
- +
- +
- +do { k++;
- +    sd=(double)k/pow(k+2,2);
- +    si=pow(-1,k);
- +    Sum+=sd*si;
- +
- +}while(sd>=E);
- +    cout <<Sum<<endl;
- +    return 0;
- +} 
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+  double E=1e-6;
+  double Sum=0;
+  double sd;
+  double si;
+  int k=0;
+ 
+ do { k++;
+     sd=(double)k/pow(k+2,2);
+     si=pow(-1,k);
+     Sum+=sd*si;
+ 
+ }while(sd>=E);
+   cout <<Sum<<endl;
+     return 0;
+ } 
