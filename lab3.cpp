12b71eb530c786d71e66679692c2992b63efdc85
LeakySum <leakysum60@gmail.com>
2016-12-01 23:35:16 +0300

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -1,20 +1,19 @@
+#include "stdafx.h"
 #include <iostream>
 #include <math.h>
 using namespace std;
 
 int main() {
-  double E=1e-6;
-  double Sum=0;
-  double sd;
-  double si;
-  int k=0;
- 
- do { k++;
-     sd=(double)k/pow(k+2,2);
-     si=pow(-1,k);
-     Sum+=sd*si;
- 
- }while(sd>=E);
-   cout <<Sum<<endl;
-     return 0;
- } 
+	double E = 1e-6, Sum = 0, val_now, sign;
+ 	int k = 0;
+
+	do {
+		k++;
+		val_now = (double)k / pow(k + 2, 2);
+		sign = pow(-1, k);
+		Sum += val_now*sign;
+
+	} while (val_now >= E);
+	cout <<"Значение суммы ряда чисел" << Sum << endl;
+	system("pause");
+}
