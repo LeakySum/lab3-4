9282a73937415f2e7d4905e07e86833d06005b47
LeakySum <leakysum60@gmail.com>
2016-12-01 23:35:36 +0300

    Update lab4-1.cpp

--- lab4-1.cpp
+++ lab4-1.cpp
@@ -1,35 +1,32 @@
+#include "stdafx.h"
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
 
 int main() {
-    int *M;
-    int n, k, i, j;
-    cout << "n=";
-    cin >> n;
-    M=(int*) malloc(n*sizeof(int));
+	int *M;
+	int n, k, i, j;
+	cout << "enter the number of elements n=";
+	cin >> n;
+	M = (int*)malloc(n * sizeof(int));
 
-    for (i=0; i<n; i++){
-        M[i] = rand() % 15 +10;
-    }
+	for (i = 0; i<n; i++) {
+		cin >> M[i];
+	}
+	
+	for (i = 0; i<n; i++) {
+		for (j = 0; j < (n - 1); j++) {
+			if (M[j] > M[j + 1]) {
+				k = M[j];
+				M[j] = M[j + 1];
+				M[j + 1] = k;
+			}
+		}
 
-    for (i=0; i<n; i++){
-        cout <<  M[i] <<" ";
-    }
-cout << endl;
-    for ( i=0; i<n; i++) {
-        for ( j = 0; j < (n-1); j++) {
-            if (M[j] > M[j + 1]) {
-                k = M[j];
-                M[j] = M[j+1];
-                M[j+1] = k;
-            }
-        }
-
-    }
-    for (i=0; i<n; i++){
-      cout <<  M[i] <<" ";
-    }
-
-    return 0;
+	}
+	for (i = 0; i<n; i++) {
+		cout << M[i] << " ";
+	}
+	cout << endl;
+	system("pause");
 }
